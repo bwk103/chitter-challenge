b02945238aae40d7174037fe8b6da98d7e618c1d
Ben Kielty <bwk103@gmail.com>
2018-01-16 12:11:28 +0000

    Refactor data_mapper_setup
    Refactored setup so that heroku can access db.  Minor amends to peep
    view.

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -4,5 +4,5 @@ require 'dm-postgres-adapter'
 require_relative 'models/peep'
 require_relative 'models/user'
 
-DataMapper.setup(:default, "postgres://localhost/chitter_challenge_#{ENV['RACK_ENV']}")
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/chitter_challenge_#{ENV['RACK_ENV']}")
 DataMapper.finalize
